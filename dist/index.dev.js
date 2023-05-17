6fee2c62b217729c415f5c47ab7746e21c335e0c
Muhammad Hamza Sohail <muhammadhamzasohail@Muhammads-MacBook-Air.local>
2023-05-17 21:13:57 +0300

    Create - test

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -11,14 +11,43 @@ var credentials = require('./key.json');
 admin.initializeApp({
   credentials: admin.credential.cert(credentials)
 });
-var db = admin.firestore();
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
+var db = admin.firestore();
+app.post('/create', function _callee(req, res) {
+  var id, labjson, response;
+  return regeneratorRuntime.async(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          try {
+            console.log(req.body.email);
+            id = req.body.email;
+            labjson = {
+              email: req.body.email,
+              university: req.body.university,
+              department: req.body.department,
+              lab: req.body.lab,
+              area: req.body.area
+            };
+            response = db.collection("users").doc(id).set(labjson);
+            res.send(response);
+          } catch (error) {
+            res.send(error);
+          }
+
+        case 1:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+});
 app.get("/home", function (req, res) {
-  res.send("Hello we are Lab2Client");
+  res.send("Hello we are Lab2Client Team");
 });
 app.listen(5000, function () {
-  console.log('Listening on port 5000');
+  console.log('http://localhost:5000/');
 });
