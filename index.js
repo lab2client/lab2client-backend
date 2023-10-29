a0892aa47d989ca4a693a1ef31d63e60364a2604
Muhammad Hamza Sohail <hamza.sohail29@gmail.com>
2023-10-29 06:23:09 -0400

    Sent & Received Endpoint Fix

--- index.js
+++ index.js
@@ -856,24 +856,6 @@ app.get('payment/balance', async (req, res) => {
   });
 
 
-  app.get('/recievedpayment/:field', async (req, res) => {
-	try {
-		const user_search = req.params.field;
-		const userRef = db.collection('transactions');
-		const snapshot = await userRef.where('customerId', '==', user_search).get();
-	
-		let array = [];
-		snapshot.forEach((doc) => {
-			array.push(doc.data());
-		});
-	
-		res.send(snapshot);
-	} catch (error) {
-		res.status(500).send(error);
-	}
-	
-});
-
 app.get('/payment/sent/:field', async (req, res) => {
 	try {
 		const user_search = req.params.field;
